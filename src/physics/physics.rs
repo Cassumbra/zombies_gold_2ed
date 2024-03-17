6d4cd958d1b401f0514896d998176b014de2cb52
unerringCaprition <penumbruh@gmail.com>
2024-03-17 03:57:35 -0500

    get ourselves some normals

--- src/physics/physics.rs
+++ src/physics/physics.rs
@@ -28,8 +28,9 @@ pub fn do_physics(
                         if let Some(chunk_entity) = chunk_map.get(&chunk_position) {
                             if let Ok(chunk) = chunk_query.get(*chunk_entity) {
                                 if chunk[block_position].id != BlockID::Air {
-                                    if collider.get_intersection(transform.translation, BLOCK_AABB, global_block_position.as_vec3()) {
-                                        println!("Colliding at {}", global_block_position);
+                                    let (penetration, normal) = collider.get_penetration_and_normal(transform.translation, BLOCK_AABB, global_block_position.as_vec3());
+                                    if normal != Vec3::ZERO {
+                                        println!("Penetration: {}, Normal: {}", penetration, normal);
                                     }
                                 }
                                 continue;
@@ -71,6 +72,47 @@ impl AabbCollider {
         self_max.y >= other_min.y &&
         self_min.z <= other_max.z &&
         self_max.z >= other_min.z
+    }
+
+    pub fn get_penetration_and_normal(&self, position: Vec3, other_aabb: AabbCollider, other_position: Vec3) -> (f32, Vec3) {
+        let self_min = position - Vec3::new(self.width, self.height, self.length) / 2.0;
+        let self_max = position + Vec3::new(self.width, self.height, self.length) / 2.0;
+
+        let other_min = other_position - Vec3::new(other_aabb.width, other_aabb.height, other_aabb.length) / 2.0;
+        let other_max = other_position + Vec3::new(other_aabb.width, other_aabb.height, other_aabb.length) / 2.0;
+
+        let colliding = self_min.x <= other_max.x &&
+                        self_max.x >= other_min.x &&
+                        self_min.y <= other_max.y &&
+                        self_max.y >= other_min.y &&
+                        self_min.z <= other_max.z &&
+                        self_max.z >= other_min.z;
+
+        if !colliding {
+            return (0.0, Vec3::ZERO);
+        }
+
+        // No idea how to properly credit this but: https://research.ncl.ac.uk/game/mastersdegree/gametechnologies/physicstutorials/4collisiondetection/Physics%20-%20Collision%20Detection.pdf
+
+        let faces = vec![Vec3::X, Vec3::NEG_X,
+                         Vec3::Y, Vec3::NEG_Y,
+                         Vec3::Z, Vec3::NEG_Z];
+
+        let distances = vec![other_max.x - self_min.x, self_max.x - other_min.x,
+                             other_max.y - self_min.y, self_max.y - other_min.y,
+                             other_max.z - self_min.z, self_max.z - other_min.z];
+
+        let mut penetration = f32::MAX;
+        let mut normal = Vec3::ZERO;
+
+        for (i, face) in faces.iter().enumerate() {
+            if distances[i] < penetration {
+                penetration = distances[i];
+                normal = *face;
+            }
+        }
+
+        return(penetration, normal);
     }
 }
 
