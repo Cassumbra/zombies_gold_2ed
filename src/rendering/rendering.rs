82c3fa861b05f3c1f737119ddf1a60aabc2ac6ef
unerringCaprition <penumbruh@gmail.com>
2024-03-30 20:06:48 -0500

    add some water
    this is a dirty ugly way to be doing this, but who cares
    i can tidy things up later

--- src/rendering/rendering.rs
+++ src/rendering/rendering.rs
@@ -61,6 +61,8 @@ pub fn update_chunk_meshes (
 
         // TODO: Default to FULL instead and optimize by providing a buffer where possible
         let mut voxels = [EMPTY; ChunkShape::SIZE as usize];
+
+        let mut water_voxels = [EMPTY; ChunkShape::SIZE as usize];
         
         let offset = **ev * CHUNK_SIZE;
 
@@ -76,13 +78,24 @@ pub fn update_chunk_meshes (
                     crate::BlockID::Air => EMPTY,
                     // TODO: We should make this use our attributes, later, once we have more blocks that are translucent.
                     crate::BlockID::Leaves => TRANSLUCENT,
-                    crate::BlockID::Water => TRANSLUCENT,
+                    crate::BlockID::Water => EMPTY,
                     _ => FULL
                 }
             }
             else {
                 FULL
+            };
+
+            water_voxels[ChunkShape::linearize([(x + 1) as u32, (y + 1) as u32, (z + 1) as u32]) as usize] = 
+            if let Some(chunk) = chunk_map.get(&chunk_position) {
+                match chunk.blocks[block_position].id {
+                    crate::BlockID::Water => TRANSLUCENT,
+                    _ => EMPTY
+                }
             }
+            else {
+                FULL
+            };
         }
 
         let faces = RIGHT_HANDED_Y_UP_CONFIG.faces;
@@ -184,11 +197,6 @@ pub fn update_chunk_meshes (
         material.alpha_mode = AlphaMode::Mask(0.0);
         material.base_color_texture = Some(atlas.res_8x8.clone());
 
-
-        // Some quads were generated.
-        //assert!(buffer.num_quads() > 0);
-        //println!("quads: {}", buffer.num_quads());
-
         // TODO: Use insert_unique to insert a PbrBundle once insert_unique is available.
         /*
         commands.entity(entity).insert(PbrBundle{
@@ -206,6 +214,96 @@ pub fn update_chunk_meshes (
         });
         */
         commands.entity(chunk_map[&**ev].render_entity.unwrap())
+            .try_insert(mesh_handle)
+            .try_insert(materials.add(material))
+            .try_insert(Visibility::default())
+            .try_insert(InheritedVisibility::default())
+            .try_insert(ViewVisibility::default())
+            // TODO: This is a bandaid fix. Bevy isn't frustum culling correctly and we should properly fix it instead of just disabling it. Oh well.
+            .try_insert(NoFrustumCulling);
+
+
+
+
+
+        let mut buffer = UnitQuadBuffer::new();
+        visible_block_faces(
+            &water_voxels,
+            &ChunkShape {},
+            [0; 3],
+            [CHUNK_SIZE as u32 + 1, CHUNK_SIZE as u32 + 1, CHUNK_SIZE as u32 + 1],
+            &faces,
+            &mut buffer,
+        );
+
+        //println!("unitquadbuffer: {:?}", buffer.groups.clone());
+
+        let num_indices = buffer.num_quads() * 6;
+        let num_vertices = buffer.num_quads() * 4;
+        let mut indices = Vec::with_capacity(num_indices);
+        let mut positions = Vec::with_capacity(num_vertices);
+        let mut normals = Vec::with_capacity(num_vertices);
+        let mut uvs = Vec::with_capacity(num_vertices);
+        for (group, face) in buffer.groups.into_iter().zip(faces.into_iter()) {
+            for quad in group.into_iter() {
+                indices.extend_from_slice(&face.quad_mesh_indices(positions.len() as u32));
+                positions.extend_from_slice(&face.quad_mesh_positions(&quad.into(), 1.0));
+                normals.extend_from_slice(&face.quad_mesh_normals());
+
+                let block = chunk_map[&**ev].blocks[UVec3::from(quad.minimum) - UVec3::new(1, 1, 1)];
+                let attributes = block.get_attributes();
+                let normal = face.signed_normal();
+
+                let mut tex_coord = if normal.x == 1 {attributes.tex_coords.east}
+                             else if normal.x == -1 {attributes.tex_coords.west}
+                             else if normal.y == 1 {attributes.tex_coords.top}
+                             else if normal.y == -1 {attributes.tex_coords.bottom}
+                             else if normal.z == 1 {attributes.tex_coords.north}
+                             else {attributes.tex_coords.south};
+
+                tex_coord.x += block.damage as i32;
+
+                
+                let quad_uvs = face.tex_coords(RIGHT_HANDED_Y_UP_CONFIG.u_flip_face, true, &UnorientedQuad::from(quad)).map(|uv| {
+                    let mut u = uv[0] * 8.0;
+                    let mut v = uv[1] * 8.0;
+                    u += tex_coord.x as f32 * 8.0;
+                    v += tex_coord.y as f32 * 8.0;
+                    [u/256.0, v/256.0]
+                });
+
+                uvs.extend_from_slice(&quad_uvs);
+            }
+        }
+
+        let mut render_mesh = Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD);
+        render_mesh.insert_attribute(
+            Mesh::ATTRIBUTE_POSITION,
+            VertexAttributeValues::Float32x3(positions),
+        );
+        render_mesh.insert_attribute(
+            Mesh::ATTRIBUTE_NORMAL,
+            VertexAttributeValues::Float32x3(normals),
+        );
+        render_mesh.insert_attribute(
+            Mesh::ATTRIBUTE_UV_0,
+            VertexAttributeValues::Float32x2(uvs),
+        );
+        render_mesh.insert_indices(Indices::U32(indices.clone()));
+
+        //println!("{:?}", render_mesh);
+
+        render_mesh.translate_by(Vec3::new(-1.5, -1.5, -1.5));
+
+        let mesh_handle = meshes.add(render_mesh.clone());
+
+        // TODO: Should we be reusing this material instead of remaking it every time?
+        let mut material = StandardMaterial::from(Color::WHITE);
+        material.unlit = true;
+        material.alpha_mode = AlphaMode::Blend;
+        material.base_color_texture = Some(atlas.res_8x8.clone());
+
+        commands.entity(chunk_map[&**ev].water_render_entity.unwrap())
             .try_insert(mesh_handle)
             .try_insert(materials.add(material))
             .try_insert(Visibility::default())
