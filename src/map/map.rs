9f44ba0a5e4e2a9a4b479f8fbec978d62fab131f
unerringCaprition <penumbruh@gmail.com>
2025-07-16 01:18:33 -0700

    generate chunks BLAZINGLY FAST
    😎
    the code is getting more and more spaghettified but at least the olive oil makes it slick....

--- src/map/map.rs
+++ src/map/map.rs
@@ -18,6 +18,7 @@ use crate::sparse_grid3::SparseGrid3;
 const SEA_LEVEL: f64 = -0.0;
 
 
+
 //Plugin
 #[derive(Default)]
 pub struct MapPlugin;
@@ -31,6 +32,7 @@ impl Plugin for MapPlugin {
             .init_resource::<ChunkUnloadingQueue>()
             .init_resource::<ChunkSavingQueue>()
             .init_resource::<ChunkStatusMap>()
+            .init_resource::<FunnyMapConsts>()
             .add_event::<BlockUpdateEvent>()
             .add_event::<LoadChunkEvent>()
             .add_event::<LoadReasonChangeEvent>()
@@ -57,10 +59,12 @@ pub fn generate_chunks (
     partial_save_map: Res<ChunkSavingQueue>,
     mut loading_queue: ResMut<ChunkLoadingQueue>,
     mut chunk_status_map: ResMut<ChunkStatusMap>,
+    funny_map_consts: Res<FunnyMapConsts>,
     //time: Res<Time>,
 
     //mut next_mapgen_state: ResMut<NextState<MapGenState>>,
 ) {
+
     let start = Instant::now();
 
     let gradient = SingleDirectionAxialGradient { values: vec![1.0, 0.0, -0.5], points: vec![-(CHUNK_SIZE) as f64, 0.0, (WORLD_HEIGHT * CHUNK_SIZE) as f64], dimension: 1 };
@@ -77,7 +81,7 @@ pub fn generate_chunks (
 
     //let mut chunks_to_load = Vec::new();
 
-    **loading_queue = VecDeque::from_iter(loading_queue.iter().filter_map(|ev| if !chunk_map.contains_key(&ev.chunk) {Some(*ev)} else {None}));
+    //**loading_queue = VecDeque::from_iter(loading_queue.iter().filter_map(|ev| if !chunk_map.contains_key(&ev.chunk) {Some(*ev)} else {None}));
 
     for ev in evr_load_chunk.read() {
         // Ignore chunks that are out of generation scope.
@@ -184,6 +188,16 @@ pub fn generate_chunks (
 
                 let mut all_air = true;
                 let mut all_stone = true;
+                for (i, position) in funny_map_consts.chunk_perimeter_indices.iter() {
+                    set_block_val(*position, &mut chunk.blocks.data[*i]);
+                    if chunk.blocks.data[*i].id != BlockID::Air {
+                        all_air = false;
+                    }
+                    if chunk.blocks.data[*i].id != BlockID::Stone {
+                        all_stone = false
+                    }
+                }
+                /*
                 for (position, block_val) in chunk.blocks.iter_3d_mut() {
                     // TODO: If this works, lets get some compile-time list we can use to iterate over. that would be much faster. (probably)
                     if position.max_element() == CHUNK_SIZE - 1 || position.min_element() == 0 {
@@ -196,13 +210,10 @@ pub fn generate_chunks (
                         }
                     }
                 }
-
+                */
                 if !(all_air || all_stone) {
-                    // TODO: If this works, lets get some compile-time list we can use to iterate over. that would be much faster. (probably)
-                    for (position, block_val) in chunk.blocks.iter_3d_mut() {
-                        if position.max_element() != CHUNK_SIZE - 1 || position.min_element() != 0 {
-                            set_block_val(position, block_val);
-                        }
+                    for (i, position) in funny_map_consts.chunk_volume_indices.iter() {
+                        set_block_val(*position, &mut chunk.blocks.data[*i]);
                     }
                 }
 
@@ -210,34 +221,6 @@ pub fn generate_chunks (
                     chunk.blocks = Grid3::filled(Block::new(BlockID::Stone), [CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE]);
                 }
 
-                // Set initial values
-                /*
-                for (position, block_val) in chunk.blocks.iter_3d_mut() {
-                    let point = DVec3::from(offset + position);
-                
-                    let noise_val = noise_gen.get([point.x, point.y, point.z]);
-                    if noise_val >= 0.0 {
-                        *block_val = Block::new(BlockID::Dirt);
-                        // Make our block grass instead of dirt if the block above is air.
-                        if noise_gen.get([point.x, point.y + 1.0, point.z]) < 0.0 && point.y > 0.0 {
-                            *block_val = Block::new(BlockID::Grass);
-
-                            // Tree!
-                            if tree_noise.get([point.x, point.z]) > 0.80 {
-                                evw_gen_tree.send(GenerateTreeEvent(IVec3::new(point.x as i32, point.y as i32 + 1, point.z as i32)));
-                                // TODO: Maybe we want to do this in the tree generation system?
-                                *block_val = Block::new(BlockID::Dirt);
-                            }
-                        }
-                        if (noise_gen.get([point.x, point.y + 5.0, point.z]) > 0.0) || point.y < -5.0 {
-                            *block_val = Block::new(BlockID::Stone);
-                        }
-                    }
-                    if point.y < 0.0 && block_val.id == BlockID::Air {
-                        *block_val = Block::new(BlockID::Water)
-                    }
-                }
-                */
                 if let Some(pending_chunk) = pending_map.get_mut(&ev.chunk) {
                     for (position, modification) in pending_chunk.iter_3d_mut() {
                         if !modification.yield_to_terrain || chunk.blocks[position].id == BlockID::Air {
@@ -280,7 +263,7 @@ pub fn generate_chunks (
         }
         //println!("time to load chunk: {:?}", start_load_chunk.elapsed());
     }
-    println!("chunks loaded this frame: {}", chunks_loaded);
+    //println!("chunks loaded this frame: {}", chunks_loaded);
     //next_mapgen_state.set(MapGenState::TempBand);
 }
 
@@ -660,6 +643,33 @@ pub fn update_chunk_loaders (
                 //chunk_status_map.insert(*p, ChunkStatus::Loading);
             }
         }
+    }
+}
+
+#[derive(Clone, Resource)]
+pub struct FunnyMapConsts {
+    chunk_perimeter_indices: Vec<(usize, IVec3)>, //[(usize, IVec3); (CHUNK_SIZE.pow(3) - (CHUNK_SIZE-2).pow(3)) as usize],
+    chunk_volume_indices: Vec<(usize, IVec3)>, //[(usize, IVec3); (CHUNK_SIZE-2).pow(3) as usize],
+}
+impl Default for FunnyMapConsts {
+    fn default() -> Self {
+        Self { chunk_perimeter_indices: Grid3::<u8>::new([CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE]).iter_3d().enumerate().filter_map(|(i, (pos, _))| {
+                if pos.max_element() == CHUNK_SIZE - 1 || pos.min_element() == 0 {
+                    Some((i, pos))
+                }
+                else {
+                    None
+                }
+            }).collect::<Vec<(usize, IVec3)>>(),
+            chunk_volume_indices: Grid3::<u8>::new([CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE]).iter_3d().enumerate().filter_map(|(i, (pos, _))| {
+                if pos.max_element() != CHUNK_SIZE - 1 || pos.min_element() != 0 {
+                    Some((i, pos))
+                }
+                else {
+                    None
+                }
+            }).collect::<Vec<(usize, IVec3)>>(),
+         }
     }
 }
 
